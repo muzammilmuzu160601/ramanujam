5d6e80d0e4051f0ffdd01184b4a575849cefaeda
user <user@welcome>
2022-01-31 16:52:56 +0530

    my first commit

---
+++ mit-ramanujam/src/displayprimenumber/DisplayPrimeNumber.java
@@ -0,0 +1,31 @@
+package displayprimenumber;
+
+public class DisplayPrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i = 0;
+		    int num = 0;
+		    String primeNumbers = "";
+		   for (i=1; i<=100; i++)
+		    {
+		    	int counter=0;
+		    	for(num = i; num>=1; num--)
+		    	{
+		    		if(i%num==0)
+		    		{
+		    			counter = counter + 1;
+		    		}
+		    	}
+		    	if (counter ==2)
+		    	{
+		    		primeNumbers = primeNumbers + i + "";
+		    	}
+		    }
+		    System.out.println("Prime Numbers 1 to 100 are: ");
+		    System.out.println(primeNumbers);
+		    
+			}
+	}
+
+
